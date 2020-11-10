fcbc6aab3cf52325cf764eccbbb5b2542dd619e8
dravindranath <02700449@WLCB6TPM33>
2020-11-10 09:48:46 +0530

    Added new table

---
+++ new_databaseproject_1/dbo/Tables/DataFactorys_v2.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DataFactorys_v2] (
+    [DataFactoryId]     INT            IDENTITY (1, 1) NOT NULL,
+    [DataFactoryName]   NVARCHAR (200) NOT NULL,
+    [ResourceGroupName] NVARCHAR (200) NOT NULL,
+    [Description]       NVARCHAR (MAX) NULL
+);
+
